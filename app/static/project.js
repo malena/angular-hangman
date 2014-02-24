56a04d458f47b38e1ef471112b636b720eeb7f23
malena <malena.andrade@gmail.com>
2014-02-24 12:59:34 -0500

    Added random word and split word into character functions

--- app/static/project.js
+++ app/static/project.js
@@ -9,13 +9,46 @@ app.factory("Words", function(){
 	return Words;
 });
 
-app.controller("WordCtrl", ["$scope", "Words", function($scope, Words) {
+app.controller("WordCtrl", ["$scope", "Words", function ($scope, Words) {
 
 	$scope.words = Words;
 
+    $scope.randomWord = getRandomWord($scope.words);
+
+    $scope.splitArray = splitStringIntoArray($scope.randomWord);
+
+    console.log($scope.splitArray);
+
 	angular.forEach(Words, function(value, key){
-		console.log(value);
+
     });
 
 
+    // TODOF: These are general functions that can be re-used
+
+    function getRandomWord (list) {
+        var random_index = getRandomIndex(list);
+
+        if (list[random_index] === undefined){
+            return list[0];
+
+        } else {
+            return list[random_index];
+        }
+    }
+
+    function getRandomIndex(list){
+        var random = Math.round(Math.random() * (list.length) + 0);
+        return random;
+    }
+
+    function splitStringIntoArray(string){
+       return string.split("");
+    }
+
+
 }]);
+
+app.run(function($rootScope){
+   console.log('is this running?')
+});
