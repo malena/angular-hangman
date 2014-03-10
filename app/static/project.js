01e449e95c95f32a0a749b861e25b7ab30a5fabf
Malena Andrade <malena.andrade@gmail.com>
2014-03-09 20:09:09 -0400

    Refactoring, logging

--- app/static/project.js
+++ app/static/project.js
@@ -2,81 +2,71 @@ $(function() {
 	$('input.guess').focus();
 });
 
-var app = angular.module("hanglady", []);
+var app = angular.module("hanglady", ['ngAnimate']);
 
 app.factory("Word", function(){
-
     var Words = ["hello", "bye", "sock", "orange"];
-    var Word = getRandomWord(Words);
+    var stringWord = getRandomWord(Words);
+    var Word = makeArray(stringWord);
 
     function getRandomWord (list) {
-
         var random_index = getRandomIndex(list);
-
         if (list[random_index] === undefined){
             return list[0];
-
         } else {
             return list[random_index];
         }
     }
-
     function getRandomIndex(list){
-
         var random = Math.round(Math.random() * (list.length) + 0);
         return random;
     }
 
-    return Word;
+    function makeArray(stringWord){
+       return stringWord.split("");
+    }
 
+    return Word;
 });
 
 app.factory("Guess", function(){
     var Guess = {
-        letter: "",
-        match: null,
+        letter: null,
+        match: false,
+        counter: 0,
+        word: []
     };
     return Guess;
 });
 
-app.controller("WordCtrl", ["$scope", "Guess", "Word", function ($scope, Guess, Word) {
-
+app.controller("WordCtrl", ["$scope", "Word", "Guess", function ($scope, Word, Guess) {
+    $scope.word = Word;
     $scope.guess = Guess;
-    $scope.match = false;
 
-    $scope.randomWord = Word;
-    $scope.splitArray = splitStringIntoArray($scope.randomWord);
-
-    function splitStringIntoArray(string){
-       return string.split("");
-    }
-
-    $scope.$watchCollection('guess', function() {
-        if($scope.guess.match == true){
-            $scope.match = true;
-            return $scope.match;
-        }
-
+    $scope.$watch('guess.indexli', function(){
+      Guess.letter = Guess.indexli;
     });
-
 }]);
 
 app.controller("GuessCtrl", ["$scope", "Guess", "Word", function ($scope, Guess, Word){
 
+    $scope.word = Word;
     $scope.guess = Guess;
+    $scope.letter = Guess.letter;
 
-    $scope.$watchCollection('guess', function() {
-
-        if(!$scope.guess || $scope.guess.letter === ""){
-            $scope.guess.match = false;
-        } else {
-            angular.forEach(Word, function(key){
-                if($scope.guess.letter === key){
-                    $scope.guess.match = true;
-                }
-            });
-        }
+    $scope.$watchCollection('letter', function() {
+        angular.forEach($scope.word, function(key,index){
+            if($scope.letter == key){
+                Word.letter = key;
+                $scope.flip(index);
+            }
+        });
     });
+
+    $scope.flip = function(index) {
+        alert('flip card!' + index);
+        console.log($scope);
+    };
 
 }]);
 
