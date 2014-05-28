385a88d927627ebe350bb76552af841a07a51523
Malena Andrade <malena.andrade@gmail.com>
2014-05-28 18:20:48 -0400

    Focus cursor after entering letter

---
+++ app/directives/myGuessDirective.js
@@ -0,0 +1,20 @@
+app.directive('myGuessDirective', ['Guess', '$timeout', function(Guess, $timeout){
+
+    function link(scope, element, attrs) {
+
+        scope.$watchCollection('guess', function() {
+            if(scope.guess.letter == ""){
+                return; 
+            } else {
+                $timeout(function(){
+                    scope.guess.letter = "";
+                }, 1500);
+            }
+    	});
+    }
+    return {
+        link: link,
+        scope: false,
+        transclude: false
+    };
+}]);
